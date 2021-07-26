190d1a5b64890fbe295dd37baba8adb12a607a33
Dogwish <zefiru@protonmail.com>
2021-07-26 20:39:55 +0200

    Add palindromes solution

--- javascript-exercises/palindromes/palindromes.js
+++ javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = (str) => {
+	let filteredStr = str
+		.split(/[\s,.!?]/g)
+		.join('')
+		.toLowerCase();
+	let reversedStr = filteredStr.split('').reverse().join('');
+	return filteredStr === reversedStr ? true : false;
 };
 
 module.exports = palindromes;
