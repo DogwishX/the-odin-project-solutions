b2d1c40f0a6162d55bc96a29e33076184ae8af45
Dogwish <zefiru@protonmail.com>
2021-07-26 21:12:59 +0200

    Add fibonacci solution

--- javascript-exercises/fibonacci/fibonacci.js
+++ javascript-exercises/fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+// > A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. The simplest is the series 1, 1, 2, 3, 5, 8, etc.
+const fibonacci = (num) => {
+	if (num > 0) {
+		let fibArr = [0, 1];
 
+		while (fibArr.length <= num) {
+			fibArr.push(fibArr[fibArr.length - 2] + fibArr[fibArr.length - 1]);
+		}
+		return fibArr[fibArr.length - 1];
+	}
+	return 'OOPS';
 };
 
 module.exports = fibonacci;
