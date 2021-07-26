ff3885baa33d9132abc2dd160baa3a1707988fa3
Dogwish <zefiru@protonmail.com>
2021-07-26 21:38:50 +0200

    Add findTheOldest solution

--- javascript-exercises/findTheOldest/findTheOldest.js
+++ javascript-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = (arr) =>
+	arr
+		.map((person) => {
+			if (!person.yearOfDeath) {
+				person.yearOfDeath = 2021;
+			}
+			return person;
+		})
+		.sort((a, b) => {
+			return a.yearOfBirth - a.yearOfDeath - (b.yearOfBirth - b.yearOfDeath);
+		})[0];
 
 module.exports = findTheOldest;
