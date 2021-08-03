3a04ec5e0e84bd907498157f3338612863f7bfc3
Dogwish <zefiru@protonmail.com>
2021-08-03 12:56:22 +0200

    Add deleteBtn functionality

--- library/script.js
+++ library/script.js
@@ -1,12 +1,12 @@
 // Form
 const book = document.querySelector('#book-input');
 const author = document.querySelector('#author-input');
-let bookStatus = 'Read';
 const statusList = document.querySelector('#selected-item');
 const statusListOptions = document.querySelector('.options');
 const statusListLabels = document.querySelectorAll('.options label');
 const statusSelectedItem = document.querySelector('#selected-item');
 const submitBtn = document.querySelector('#submit-btn');
+let bookStatus = 'Read';
 
 submitBtn.addEventListener('click', handleSubmit);
 statusList.addEventListener('click', toggleStatusList);
@@ -16,10 +16,12 @@ statusListLabels.forEach((child) =>
 );
 
 function handleSubmit() {
-	addBookToLibrary(book.value, author.value, bookStatus);
-	book.value = '';
-	author.value = '';
-	displayLibrary();
+	if (book.value.length > 0 && author.value.length > 0) {
+		addBookToLibrary(book.value, author.value, bookStatus);
+		displayLibrary();
+		book.value = '';
+		author.value = '';
+	}
 }
 
 function toggleStatusList() {
@@ -35,15 +37,23 @@ function handleOptionSelect({ target }) {
 // Library
 
 let myLibrary = [
-	{ author: 'scott', book: 'EndersGame2', isRead: true, nPages: '365' },
-	{ author: 'scott', book: 'EndersGame3', isRead: true, nPages: '365' },
-	{ author: 'scott', book: 'EndersGame4', isRead: true, nPages: '365' },
+	// { author: 'scott', book: 'EndersGame2', isRead: 'Read', nPages: '365' },
+	// { author: 'scott', book: 'EndersGame3', isRead: 'Read', nPages: '365' },
+	// { author: 'scott', book: 'EndersGame4', isRead: 'Read', nPages: '365' },
 ];
 
-function Book(book, author, isRead) {
-	this.book = book;
-	this.author = author;
-	this.isRead = isRead;
+class Book {
+	constructor(book, author, isRead) {
+		this.book = book;
+		this.author = author;
+		this.isRead = isRead;
+	}
+	toggle() {
+		this.textContent == 'Read'
+			? (this.textContent = 'Not Read')
+			: (this.textContent = 'Read');
+		console.log(this.isRead);
+	}
 }
 
 function addBookToLibrary(book, author, isRead) {
@@ -62,17 +72,30 @@ function displayLibrary() {
 		const name = document.createElement('td');
 		const author = document.createElement('td');
 		const status = document.createElement('td');
+		const deleteBtn = document.createElement('td');
 
 		name.textContent = `${book.book}`;
 		author.textContent = `${book.author}`;
 		status.textContent = `${book.isRead}`;
-		tableRow.append(name, author, status);
+		tableRow.append(name, author, status, deleteBtn);
 		tableRow.classList.add('table-data');
 		libraryTable.append(tableRow);
+
+		status.addEventListener('click', book.toggle);
+
+		deleteBtn.textContent = `Delete`;
+		deleteBtn.id = book.book;
+		deleteBtn.classList.add('delete-btn');
+		deleteBtn.addEventListener('click', removeRow);
 	}
 }
 
 function resetDisplay() {
 	const libraryDataItems = document.querySelectorAll('.table-data');
 	libraryDataItems.forEach((item) => item.remove());
+}
+
+function removeRow({ target }) {
+	myLibrary = myLibrary.filter((item) => item.book != target.id);
+	target.parentNode.remove();
 }
