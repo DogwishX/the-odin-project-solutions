9e83ab1e0ae6e4747526d76fbd053c1d0710349b
Dogwish <zefiru@protonmail.com>
2021-08-04 17:34:17 +0200

    Add main class for future use in tabs

--- restaurant/src/index.js
+++ restaurant/src/index.js
@@ -2,40 +2,91 @@ import './style.css';
 
 const WEBPAGE = (() => {
 	class Element {
-		constructor(tag, cssSelector, parentNode) {
+		constructor(tag, cssSelector, parentNode, text = '') {
 			this.element = document.createElement(tag);
 			this.element.classList.add(cssSelector);
-			this.appendTo(parentNode);
+			this.element.textContent = text;
+			this._appendTo(parentNode);
 		}
 
-		appendTo(parentNode) {
+		_appendTo(parentNode) {
 			this.parentNode = document.querySelector(parentNode);
 			this.parentNode.appendChild(this.element);
 		}
 
-		assignText(text) {
-			this.element.textContent = text;
-		}
+		assignText(text) {}
 	}
 
 	class Header {
 		constructor() {
 			this.header = new Element('div', 'header', '#content');
-			this.mozzafiato = new Element('h1', 'mozzafiato', '.header').assignText(
+			this.mozzafiato = new Element(
+				'h1',
+				'mozzafiato',
+				'.header',
 				'Mozzafiato'
 			);
 			this.tabsContainer = new Element('ul', 'tabs__container', '.header');
-			this.createTabs();
+			this._createTabs();
 		}
 
-		createTabs() {
+		_createTabs() {
 			const tabs = ['Home', 'Menu', 'Contact'];
 			tabs.forEach((tab) => {
-				this[tab] = new Element('li', 'tabs__tab', '.tabs__container');
-				this[tab].element.textContent = tab;
+				this[tab] = new Element('li', 'tab', '.tabs__container', tab);
 			});
 		}
 	}
 
+	class Main {
+		constructor(pageName) {
+			this.mainDiv = new Element('div', 'main', '#content');
+			this[pageName]();
+		}
+		home() {
+			this.homeDiv = new Element('div', 'home', '.main');
+
+			this.firstLine = new Element(
+				'p',
+				'home-text',
+				'home',
+				'Best pizza in your country'
+			);
+
+			this.secondLine = new Element(
+				'p',
+				'home-text',
+				'home',
+				'Made with passion since 1908'
+			);
+
+			this.image = new Image();
+			this.image.src = '';
+
+			this.thirdLine = new Element(
+				'p',
+				'home-text',
+				'home',
+				'Order online or visit us!'
+			);
+		}
+		menu() {
+			console.log('menu');
+		}
+		contact() {
+			console.log('contact');
+		}
+	}
+
+	class Footer {
+		constructor() {
+			this.footer = new Element('div', 'footer', '#content');
+		}
+	}
+
 	const header = new Header();
+	const home = new Main('home');
+	// const menu = new Main('menu');
+	// const contact = new Main('contact');
+	const footer = new Footer();
 })();
