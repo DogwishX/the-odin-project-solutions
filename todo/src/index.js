a40796bd3e2b587628db941ea8d3617b553b877d
Dogwish <zefiru@protonmail.com>
2021-08-09 13:55:06 +0200

    Fix issue with making new tabs active instead of page

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,37 +2,125 @@ import './style.css';
 import Element from './component/element';
 
 const DOMCache = {
-	burger: document.querySelector('.burger'),
+	pages: document.querySelectorAll('.page'),
+	burgerBtn: document.querySelector('.burger'),
+	homeBtn: document.querySelector('.home'),
+	menu: document.querySelector('.task-pages'),
+	newProjectTabBtn: document.querySelector('.add-project'),
+	newProjectFormBtn: document.querySelector('.add-project-btn'),
+	newProjectFormInput: document.querySelector('#project-name'),
 
 	addListeners() {
-		this.burger.addEventListener('click', showMenu);
+		this.homeBtn.addEventListener('click', pages.displayHome);
+		this.burgerBtn.addEventListener('click', this.toggleMenu.bind(this), false);
+		this.newProjectTabBtn.addEventListener(
+			'click',
+			this.showNewProjectForm.bind(this)
+		);
+		this.pages.forEach((page) =>
+			page.addEventListener('click', this.hideMenu.bind(this))
+		);
+		this.updateTabListeners();
 	},
 
-	showMenu() {},
+	updateTabListeners() {
+		this.tabs = document.querySelectorAll('.tab');
+		this.tabs.forEach((tab) => {
+			tab.addEventListener('click', this.handleTabClick.bind(this));
+		});
+	},
+
+	toggleMenu() {
+		this.menu.classList.toggle('active');
+	},
+
+	hideMenu() {
+		this.menu.classList.remove('active');
+	},
+
+	toggleActiveComponent(name) {
+		const component = document.querySelector(name);
+		component.classList.toggle('active');
+	},
+
+	handleTabClick(event) {
+		pages.displayPage(event);
+		this.hideMenu();
+	},
+
+	showNewProjectForm() {
+		this.toggleActiveComponent('.new-project-form');
+		this.toggleActiveComponent('.overlay');
+		this.newProjectFormBtn.addEventListener('click', () => {
+			const projectName = DOMCache.newProjectFormInput.value;
+			const project = new Project(projectName);
+			project.createProject(projectName);
+		});
+	},
 };
 
 const pages = {
 	pagesList: ['Inbox', 'Today'],
 
-	hidePages() {
-		this.pagesList.forEach((page) => page.classList.remove('active'));
+	addPage(pageName) {
+		this.pagesList.push(pageName);
+		this.updatePages();
 	},
 
-	createPages() {
+	resetPages() {
+		const pagesArr = document.querySelectorAll('.page');
+		console.log(pagesArr);
+		pagesArr.forEach((page) => page.remove());
+	},
+
+	updatePages() {
+		this.resetPages();
 		this.pagesList.forEach((page) => {
-			const container = new Element('div', ['page', page], 'main', page);
+			const container = new Element('div', ['page', page], '.pages', page);
 		});
 	},
 
-	displayPage({ target }) {},
+	displayPage({ target }) {
+		pages.hidePages();
+		const affectedPage = document.querySelector(`.${target.textContent}`);
+		console.log(affectedPage);
+		affectedPage.classList.add('active');
+	},
+
+	displayHome() {
+		pages.hidePages();
+		document.querySelector('.Today').classList.add('active');
+	},
+
+	hidePages() {
+		this.pagesList.forEach((page) => {
+			const pageDOM = document.querySelector(`.${page}`);
+			pageDOM.classList.remove('active');
+		});
+	},
 };
 
-pages.createPages();
+pages.addPage('Projects');
+
 class Project {
 	constructor(name) {
 		this.taskArr = [];
 		this.name = name;
 	}
+	createProject(projectName) {
+		pages.addPage(projectName);
+
+		new Element(
+			'div',
+			[`${projectName}-tab`, 'tab'],
+			'.projects-tab',
+			projectName
+		);
+		DOMCache.toggleActiveComponent('.new-project-form');
+		DOMCache.toggleActiveComponent('.overlay');
+		DOMCache.updateTabListeners();
+	}
+
 	displayProject() {}
 	deleteProject() {}
 }
@@ -45,3 +133,7 @@ class Task extends Project {
 	deleteTask() {}
 	editTask() {}
 }
+
+// Run page
+DOMCache.addListeners();
+pages.updatePages();
