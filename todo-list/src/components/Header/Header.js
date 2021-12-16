613e3c456f21d71c068ec31113abcccd8bd0d206
DogwishX <zefiru@protonmail.com>
2021-12-16 20:30:13 +0000

    Add conditional rendering of menu

--- todo-list/src/components/Header/Header.js
+++ todo-list/src/components/Header/Header.js
@@ -1,6 +1,26 @@
 import React from "react";
+import { render } from "react-dom";
+import "./Header.css";
 
 class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = { windowWidth: window.innerWidth };
+    this.updateWindowSize = this.updateWindowSize.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateWindowSize);
+    // this.updateWindowSize();
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('resize', this.updateWindowSize)
+  }
+  updateWindowSize() {
+    this.setState({ windowWidth: window.innerWidth });
+  }
+
   render() {
     return (
       <div className="header">
@@ -10,6 +30,16 @@ class Header extends React.Component {
           src="https://to-do-cdn.microsoft.com/static-assets/c87265a87f887380a04cf21925a56539b29364b51ae53e089c3ee2b2180148c6/icons/logo.png"
           alt="site logo"
         />
+        <div data-testid="header__title" className="header__title">
+          Todo List
+        </div>
+        {this.state.windowWidth <= 650 ? (
+          <div data-testid="header__menu" className="header__menu">
+            Menu
+          </div>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
--- todo-list/src/components/Header/Header.test.js
+++ todo-list/src/components/Header/Header.test.js
@@ -6,8 +6,20 @@ describe("render", () => {
   afterEach(() => cleanup());
 
   test("logo", () => {
-    const logo = screen.getByTestId(/logo/gi);
+    const logo = screen.getByTestId(/header__logo/gi);
     expect(logo).toBeTruthy();
   });
+  test("title", () => {
+    const title = screen.getByTestId(/header__title/gi);
+    expect(title).toBeTruthy();
+  });
+  test("menuIcon - small screen", () => {
+    const menu = screen.getByTestId(/header__menu/gi);
+    window.innerWidth = 300;
+    expect(menu).toBeTruthy();
+  });
+//   test('menuIcon - not present in big screen', () => {
+//     const menu = screen.getByTestId(/header__menu/gi);
+//     expect(menu).toBeFalsey();
+//   })
 });
- 
