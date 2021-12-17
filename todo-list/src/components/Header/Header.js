b81568243e131f89e8cd36860d8d5a7bcf13742b
DogwishX <zefiru@protonmail.com>
2021-12-17 12:08:34 +0000

    Add menu icon animation

--- todo-list/src/components/Header/Header.js
+++ todo-list/src/components/Header/Header.js
@@ -20,22 +20,40 @@ class Header extends React.Component {
   updateWindowSize() {
     this.setState({ windowWidth: window.innerWidth });
   }
+  showMenu({ currentTarget }) {
+    currentTarget.classList.toggle("header__menu--active");
+  }
 
   render() {
     return (
       <div className="header">
-        <img
-          data-testid="header__logo"
-          className="header__logo"
-          src="https://to-do-cdn.microsoft.com/static-assets/c87265a87f887380a04cf21925a56539b29364b51ae53e089c3ee2b2180148c6/icons/logo.png"
-          alt="site logo"
-        />
         <div data-testid="header__title" className="header__title">
+          <img
+            data-testid="header__logo"
+            className="header__logo"
+            src="https://to-do-cdn.microsoft.com/static-assets/c87265a87f887380a04cf21925a56539b29364b51ae53e089c3ee2b2180148c6/icons/logo.png"
+            alt="site logo"
+          />
           Todo List
         </div>
         {this.state.windowWidth <= 650 ? (
-          <div data-testid="header__menu" className="header__menu">
-            Menu
+          <div
+            className="header__menu"
+            onClick={this.showMenu}
+            data-testid="header__menu"
+          >
+            <div className="header__menu--top">
+              <span></span>
+              <span></span>
+            </div>
+            <div className="header__menu--mid">
+              <span></span>
+              <span></span>
+            </div>
+            <div className="header__menu--bot">
+              <span></span>
+              <span></span>
+            </div>
           </div>
         ) : (
           ""
--- todo-list/src/components/Header/Header.test.js
+++ todo-list/src/components/Header/Header.test.js
@@ -1,4 +1,4 @@
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, render, screen, fireEvent } from "@testing-library/react";
 import Header from "./Header";
 
 describe("render", () => {
@@ -16,14 +16,30 @@ describe("render", () => {
     const title = screen.getByTestId(/header__title/gi);
     expect(title).toBeTruthy();
   });
-  test("menuIcon - small screen", () => {
-    window.innerWidth = 300;
-    render(<Header />);
-    const menu = screen.getByTestId(/header__menu/gi);
-    expect(menu).toBeTruthy();
-  });
   test("menuIcon - not present in big screen", () => {
     const menu = screen.queryByTestId(/header__menu/gi);
     expect(menu).toBeFalsy();
+  });
+});
+
+describe("menu - mobile", () => {
+  beforeEach(() => {
+    window.innerWidth = 300;
+    render(<Header />);
+  });
+
+  afterEach(() => {
+    cleanup();
+  });
+
+  test("display icon on small screen", () => {
+    const menu = screen.getByTestId(/header__menu/gi);
+    expect(menu).toBeTruthy();
+  });
+
+  test("animate on click", () => {
+    const menu = screen.getByTestId(/header__menu/gi);
+    fireEvent.click(menu);
+    expect(menu.className.includes("active")).toBe(true);
   });
 });
