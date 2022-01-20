29cd26c1dcecc7fac589d523d181b5e2237d60f7
DogwishX <zefiru@protonmail.com>
2022-01-20 16:14:32 +0000

    Improve logo

--- not-spotify/src/Components/Nav/Nav.js
+++ not-spotify/src/Components/Nav/Nav.js
@@ -4,6 +4,7 @@ import useToggle from "../../hooks/useToggle";
 import "./Nav.css";
 import MenuIcon from "./MenuIcon";
 import MenuItems from "./MenuItems";
+import Logo from "../Core/Logo";
 
 function Nav({ id = null, items, vertical = false }) {
   const device = useWidthQuery();
@@ -11,11 +12,11 @@ function Nav({ id = null, items, vertical = false }) {
 
   return (
     <div className={`nav${vertical ? "--vertical" : "--horizontal"}`} id={id}>
+      <Logo text="Spotify" id="nav__logo" />
       {device !== "desktop" ? (
         <>
           <MenuIcon isMenuOpen={isMenuOpen} toggle={toggleIsMenuOpen} />
           {isMenuOpen && <MenuItems items={items} />}
-          <Profile device={device} />
         </>
       ) : (
         ""
